3ff4a4164a4f533ea45665a0ced44ba3e8334eac
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-17 17:01:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+function reverseString(word){
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reverseWord =  reversedWordArray.join("");
+  return reverseWord;
+}
+
 function isPalindrome(word) {
+  const reverseWord = reverseString(word);
+
+  if(word === reverseWord) {
+    return true;
+
+  }
+  else{
+    return false;
+  }
+  
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+*/
+/*
+if the word is the same as the one in reverse, then return true
+reverse the input string
+if the input string is same as the reverse input
+
+return true
+
+else
+
+return false
 */
 
 /*
